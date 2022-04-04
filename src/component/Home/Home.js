a01c38972a29d4de1901e3b0b2ec4158a0ea79f3
ijTuhin <isratishu0209@gmail.com>
2022-04-05 03:12:48 +0600

    all reviews are loaded in the home page

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -5,6 +5,8 @@ import img from './../../img.jpg';
 import './Home.css';
 import useReview from '../../hooks/useReview';
 import { useNavigate } from 'react-router-dom';
+import LessReview from './LessReview';
+import './../Reviews/Review.css';
 const Home = () => {
     const [reviews, setReviews] = useReview();
     const navigate = useNavigate();
@@ -16,6 +18,14 @@ const Home = () => {
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium nisi ipsum, maxime delectus porro quas aut assumenda blanditiis aperiam ad provident mollitia repellat quis harum nobis earum commodi dolorem ratione quos optio voluptatem. Architecto cumque, amet suscipit ab doloribus quae impedit aperiam nisi eum rem sapiente aspernatur! Quasi quis, earum dolorem quos, quibusdam minus consectetur autem, neque voluptatum et nam.</p>
                 </div>
                 <img src={img} alt="img" />
+            </div>
+            <div className='review-container'>
+                {
+                    reviews.map(review => <LessReview
+                        key={review.id}
+                        review={review}
+                    ></LessReview>)
+                }
             </div>
             <div className='review-btn-container'>
                 <button onClick={() => navigate('/review')} className='review-btn'><span>See all reviews</span><FontAwesomeIcon icon={faArrowRightLong}></FontAwesomeIcon></button>
---
+++ src/component/Home/LessReview.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './../Reviews/Review.css';
+const LessReview = ({ review }) => {
+    const { name, reviews, ratings } = review;
+    console.log(review);
+    return (
+        <div className='review-card'>
+            <p>Name: {name}</p>
+            <p>Review: {reviews}</p>
+            <p>Ratings: {ratings} stars</p>
+        </div>
+    );
+};
+
+export default LessReview;
