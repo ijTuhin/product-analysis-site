69443f2e213007f6ba734bfc7fae9103d1d861b0
ijTuhin <isratishu0209@gmail.com>
2022-04-04 22:08:39 +0600

    chart created for dashboard

--- src/component/Dashboard/Dashboard.js
+++ src/component/Dashboard/Dashboard.js
@@ -1,9 +1,14 @@
 import React from 'react';
-
+import MyAreaChart from '../Chart/MyAreaChart';
+import MyLineChart from '../Chart/MyLineChart';
+import './Dashboard.css';
 const Dashboard = () => {
     return (
         <div>
-            <h3>Create two chart here</h3>
+            <div className='chart-container'>
+                <MyLineChart></MyLineChart>
+                <MyAreaChart></MyAreaChart>
+            </div>
         </div>
     );
 };
