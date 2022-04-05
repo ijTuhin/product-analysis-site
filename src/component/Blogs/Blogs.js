a10a5f8bc911653d90a200faf34083f87be5872f
ijTuhin <isratishu0209@gmail.com>
2022-04-05 17:03:42 +0600

    blog page done

--- src/component/Blogs/Blogs.js
+++ src/component/Blogs/Blogs.js
@@ -1,13 +1,16 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRightLong, faBarChart } from '@fortawesome/free-solid-svg-icons'
 import React from 'react';
 import './Blogs.css';
 const Blogs = () => {
     return (
         <div className='blog-container'>
+            <h1> <FontAwesomeIcon icon={faBarChart}></FontAwesomeIcon> <span style={{ color: "goldenrod" }}>Question</span> <span style={{ color: "rgba(64, 26, 56, 0.7)" }}>Answer</span></h1>
             <h3>What is Context API ?</h3>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem, dolores numquam quas, blanditiis corporis expedita assumenda facilis enim quo repellat optio eligendi nisi nulla, aut eum. Asperiores quasi rerum error quibusdam dolorem ipsum eligendi aliquam. Tempora, facilis eius quasi sapiente perferendis aspernatur. Eum eos natus alias iusto maiores doloribus ex!</p>
+            <p>The React Context API is a way for a React app to effectively produce global variables that can be passed around. It is primarily used when some data needs to be accessible by many components at different nesting levels. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on.. Context provides a way to share values between components without having to explicitly pass a prop through every level of the tree. It means, context API let a a component pass the very nested component of deep level without the help of it's upper level.</p>
             <hr />
-            <h3>Differences between Inline-blog & Inline-blog element</h3>
-            <p>Est ut veniam voluptatum temporibus earum consectetur culpa facilis fuga molestias? Ipsa corrupti perferendis nostrum numquam vero, quas porro magnam nihil error sunt excepturi doloremque, quod optio a eveniet quos veniam inventore iste commodi quam? Cumque, reprehenderit nam perferendis consectetur itaque, soluta, aliquam modi quam numquam asperiores voluptates illo in?</p>
+            <h3>What is semantic tag?</h3>
+            <p>Semantic HTML elements are those that clearly describe their meaning in a human- and machine-readable way, it also makes it easier to both the browser and the developer to understand the code. Elements such as &lt;header&gt; , &lt;footer&gt; and &lt;article&gt; are all considered semantic because they accurately describe the purpose of the element and the type of content that is inside them.Dec</p>
         </div>
     );
 };
--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -5,20 +5,20 @@ import './Home.css';
 import useReview from '../../hooks/useReview';
 import { useNavigate } from 'react-router-dom';
 import LessReview from './LessReview';
-import './../Reviews/Review.css';
 const Home = () => {
     const [reviews, setReviews] = useReview();
     const navigate = useNavigate();
     return (
         <div className='home-page'>
-            <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+            <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center", marginBottom: "70px" }}>
                 <div className='heading-text'>
                     <h1>Some <span style={{ color: "goldenrod" }}>Project</span> Name</h1>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium nisi ipsum, maxime delectus porro quas aut assumenda blanditiis aperiam ad provident mollitia repellat quis harum nobis earum commodi dolorem ratione quos optio voluptatem. Architecto cumque, amet suscipit ab doloribus quae impedit aperiam nisi eum rem sapiente aspernatur! Quasi quis, earum dolorem quos, quibusdam minus consectetur autem, neque voluptatum et nam.</p>
                 </div>
                 <img src={img} alt="img" />
             </div>
-            <div className='review-container'>
+            <h1 style={{ textAlign: "center" }}>What our customers say?</h1>
+            <div className='less-review-container'>
                 {
                     reviews.slice(0, 3).map(review => <LessReview
                         key={review.id}
