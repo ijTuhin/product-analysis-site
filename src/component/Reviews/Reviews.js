39727e19b8962a64a8e775bf50772b128cf82418
ijTuhin <isratishu0209@gmail.com>
2022-04-05 20:46:10 +0600

    review heading designed

--- src/component/Reviews/Reviews.js
+++ src/component/Reviews/Reviews.js
@@ -1,18 +1,23 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleDoubleRight, faBarChart } from '@fortawesome/free-solid-svg-icons'
 import useReview from '../../hooks/useReview';
 import EachReview from './EachReview';
 import './Review.css';
 const Reviews = () => {
-    //Load data
+    //Load data 
     const [reviews, setReviews] = useReview();
 
     return (
-        <div className='review-container'>
-            {
-                reviews.map(review => <EachReview
-                    key={review.id}
-                    review={review}
-                ></EachReview>)
-            }
+        <div className='reviews'>
+            <h2><span style={{ color: "#6b2f66" }}><FontAwesomeIcon icon={faBarChart}></FontAwesomeIcon></span> Customer <span style={{ color: "#87a353" }}>Reviews  <FontAwesomeIcon icon={faAngleDoubleRight}></FontAwesomeIcon></span></h2>
+            <div className='review-container'>
+                {
+                    reviews.map(review => <EachReview
+                        key={review.id}
+                        review={review}
+                    ></EachReview>)
+                }
+            </div>
         </div>
     );
 };
