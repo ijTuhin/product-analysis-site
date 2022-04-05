f6037ac40c81d2bcb0b5adb7c35a9b6b641cf552
ijTuhin <isratishu0209@gmail.com>
2022-04-05 22:10:26 +0600

    designed about us

--- src/component/About/About.js
+++ src/component/About/About.js
@@ -1,9 +1,13 @@
 import React from 'react';
-
+import aboutUs from './../../images/about-us.jpg';
+import './../Home/Home.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleDoubleRight, faMailBulk, faMailForward, faMessage } from '@fortawesome/free-solid-svg-icons'
 const About = () => {
     return (
-        <div>
-            <h5>About us</h5>
+        <div className='about-us'>
+            <img src={aboutUs} alt="" />
+            <button className='contact-btn'>Contact Us <span><FontAwesomeIcon icon={faMailForward}></FontAwesomeIcon></span></button>
         </div>
     );
 };
